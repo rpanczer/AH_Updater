3b0c461e9753481db8d584e011bcd9f0d89f4b96
rpanczer <bobby.panczer@arcadis-us.com>
2017-10-22 15:12:31 -0400

    src: remove browseFile from Application class

--- src/view.py
+++ src/view.py
@@ -1,21 +1,22 @@
 import tkinter as tk
-import tkFileDialog as tkfd
+from tkinter import filedialog as fd
 import os
 
 
 class Application(tk.Frame):
+
+    filename = "hi"
+
     def __init__(self, master=None):
         tk.Frame.__init__(self, master)
         self.grid()
 
         # Instantiate widgets
         self.filePathDisplay = tk.Entry(self)
-        self.browseButton = tk.Button(self, text='Browse...')
+        self.browseButton = tk.Button(self, text='Browse...', command=browseFile)
         self.runButton = tk.Button(self, text='Run', fg='white',background='green')
         self.quitButton = tk.Button(self, text='Quit', fg='white', background='red', command=self.quit)
-
         self.createWidgets()
-        self.browseFile()
 
     def createWidgets(self):
         # Arrange widgets on the grid layout
@@ -24,6 +25,6 @@ class Application(tk.Frame):
         self.runButton.grid(row=1, column=0)
         self.quitButton.grid(row=1, column=3)
 
-    def browseFile(self):
-        filename = tkfd.askopenfilename(filetypes = ("temp", "*.aht"))
-        print filename
+
+def browseFile():
+    print(Application.filename)
