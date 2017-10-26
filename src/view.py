8be5a0128474049c4a76fcfa75ec5db71cbdee9f
rpanczer <bobby.panczer@arcadis-us.com>
2017-10-25 22:39:54 -0400

    view: edits to create folder

--- src/view.py
+++ src/view.py
@@ -1,7 +1,7 @@
 import tkinter as tk
-from tkinter import filedialog as fd
 import os
-
+import zipfile
+import datetime
 
 class Application(tk.Frame):
 
@@ -13,22 +13,37 @@ class Application(tk.Frame):
 
         # Instantiate widgets
         self.filePathDisplay = tk.Entry(self)
-        self.browseButton = tk.Button(self, text='Browse...', command=browseFile)
+        self.browseButton = tk.Button(self, text='Browse...', command=createfolder)
         self.runButton = tk.Button(self, text='Run', fg='white', background='green')
         self.quitButton = tk.Button(self, text='Quit', fg='white', background='red', command=self.quit)
-        self.createWidgets()
+        self.createwidgets()
 
-    def createWidgets(self):
+    def createwidgets(self):
         # Arrange widgets on the grid layout
         self.filePathDisplay.grid(row=0, column=0)
         self.browseButton.grid(row=0, column=3)
         self.runButton.grid(row=1, column=0)
         self.quitButton.grid(row=1, column=3)
-
+"""
     def browseFile():
         print(Application.filename)
         foldername = "templates/{}".filename
         global templatepath = "templates_todeploy/{}".format(foldername)
         createFolder()
-
-    def createFolder(templatepath):
+"""
+def createfolder():
+    #Make the path for the template folder general at some point
+    path = "C:/Users/rpanczer/PyCharmProjects/AH_Updater/templates_archive/template"
+    foldercheck = os.path.exists(path)
+    if foldercheck == 0:
+        os.mkdir(path, 0o755)
+        print("trace :: Template folder created")
+    else:
+        print("trace :: Template folder already exists")
+    now = datetime.datetime.now()
+    zipsyntax = str(now.day) + "_" + str(now.month) + "_" + str(now.year)
+    print(zipsyntax)
+    """
+    zippath = "{}".format(zipsyntax)
+    zipfile.ZipFile(zippath, mode="r", compression= ZIP_STORED, allowZip64= true)
+    """
